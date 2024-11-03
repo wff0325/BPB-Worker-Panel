b38629467ebca7f7496d56631f4d041d4f664f14
wff0325 <140928525+wff0325@users.noreply.github.com>
2024-11-03 19:53:22 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4085,7 +4085,7 @@ var SignJWT = class extends ProduceJWT {
 };
 
 // src/worker.js
-var userID = "89b3cbba-e6ac-485a-9481-976a0415eab9";
+var userID = "83073cef-a644-4a70-8b6c-4a2c25023ed7";
 var trojanPassword = `bpb-trojan`;
 var proxyIPs = ["bpb.yousef.isegaro.com"];
 var defaultHttpPorts = ["80", "8080", "2052", "2082", "2086", "2095", "8880"];
